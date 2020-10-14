be96a2354d4b62683cfeeebb866df52860631ad4
Pim Kunis <pim@kunis.nl>
2020-10-14 20:06:30 +0200

    Add demo for adding events with liveview

--- lib/midi_sequencer_web/live/page_live.ex
+++ lib/midi_sequencer_web/live/page_live.ex
@@ -5,7 +5,7 @@ defmodule MIDISeqWeb.PageLive do
   def mount(_params, _session, socket) do
     events = test_events()
     MIDIPlayer.generate_schedule(MIDISeq.Player, events, 3000)
-    {:ok, assign(socket, events: test_events())}
+    {:ok, assign(socket, events: test_events(), adding: nil)}
   end
 
   @impl true
@@ -13,6 +13,70 @@ defmodule MIDISeqWeb.PageLive do
     MIDIPlayer.play(MIDISeq.Player)
 
     {:noreply, socket}
+  end
+
+  def handle_event("toggle-note-form", _value, %{assigns: %{adding: :note}} = socket) do
+    {:noreply, assign(socket, adding: nil)}
+  end
+
+  def handle_event("toggle-note-form", _value, socket) do
+    {:noreply, assign(socket, adding: :note)}
+  end
+
+  def handle_event(
+        "toggle-change-program-form",
+        _value,
+        %{assigns: %{adding: :change_program}} = socket
+      ) do
+    {:noreply, assign(socket, adding: nil)}
+  end
+
+  def handle_event("toggle-change-program-form", _value, socket) do
+    {:noreply, assign(socket, adding: :change_program)}
+  end
+
+  def handle_event(
+        "save_note",
+        %{
+          "note" => %{
+            "channel" => channel,
+            "tone" => tone,
+            "velocity" => velocity,
+            "end_time" => end_time,
+            "start_time" => start_time
+          }
+        },
+        %{assigns: %{events: events}} = socket
+      ) do
+    channel = String.to_integer(channel)
+    tone = String.to_integer(tone)
+    velocity = String.to_integer(velocity)
+    end_time = String.to_integer(end_time)
+    start_time = String.to_integer(start_time)
+    event = MIDIPlayer.Event.note(channel, tone, start_time, end_time, velocity)
+    events = [event | events]
+    MIDIPlayer.generate_schedule(MIDISeq.Player, events, 3000)
+    {:noreply, assign(socket, events: events, adding: nil)}
+  end
+
+  def handle_event(
+        "save_change_program",
+        %{
+          "change_program" => %{
+            "channel" => channel,
+            "time" => time,
+            "program" => program
+          }
+        },
+        %{assigns: %{events: events}} = socket
+      ) do
+    channel = String.to_integer(channel)
+    time = String.to_integer(time)
+    program = String.to_integer(program)
+    event = MIDIPlayer.Event.change_program(channel, time, program)
+    events = [event | events]
+    MIDIPlayer.generate_schedule(MIDISeq.Player, events, 3000)
+    {:noreply, assign(socket, events: events, adding: nil)}
   end
 
   defp test_events do
